636c6eff82d3a2d64a24c5794576526a53be4e1b
Nishant <nishantkheterpal@gmail.com>
2018-08-26 20:24:19 -0700

    re-register errors

--- runner2.py
+++ runner2.py
@@ -1,12 +1,13 @@
+import gym
+from gym.envs.registration import register
+
 import ray
 from ray.tune.registry import register_env
-from gym.envs.registration import register
 from ray.rllib import ppo
 
-register(id='LQREnv-v0', entry_point='gym_lqr.envs:LQREnv')
 
 def env_creator(env_config):
-    import gym
+    register(id='LQREnv-v0', entry_point='gym_lqr.envs:LQREnv')
     return gym.make("LQREnv-v0")  # or return your own custom env
 
 register_env("my_env", env_creator)
